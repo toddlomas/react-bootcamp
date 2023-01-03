90e6ada11d7eca8e8c44f50d8ba3b08cecdb8bea
Todd Lomas <todd.lomas@hotmail.co.uk>
2023-01-03 11:54:47 +0000

    Expense tracker

---
+++ react-complete-guide/src/components/UI/Buttons.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Button, Space } from "antd";
+const Buttons = () => (
+  <Space wrap>
+    <Button type="primary" ghost></Button>
+  </Space>
+);
+export default Buttons;
